2c01e705ef1ba1685b289b21e9415df28132e3f3
liuyang123654 <95159133+liuyang123654@users.noreply.github.com>
2024-05-16 12:46:35 +0800

    Add files via upload
    观察者

--- src/main/java/com/bosssoft/basic/ability/IObserver.java
+++ src/main/java/com/bosssoft/basic/ability/IObserver.java
@@ -3,5 +3,5 @@ package com.bosssoft.basic.ability;
 import java.nio.channels.Channel;
 
 public interface IObserver {
-    public void process (Channel channel, Object data);
+    public void update (ISubject subject);
 }
